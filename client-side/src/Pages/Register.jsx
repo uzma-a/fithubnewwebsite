6050ad144ca07af4981643ba3abf65548644dac6
Uzma-Aashiya <149761349+uzma-a@users.noreply.github.com>
2025-01-28 18:44:08 +0530

    Update Register.jsx

--- client-side/src/Pages/Register.jsx
+++ client-side/src/Pages/Register.jsx
@@ -36,7 +36,7 @@ const Register = () => {
 
         try {
             const response = await axios.post(
-                "http://localhost:5005/register",
+                "https://fithub-backend.vercel.app/register",
                 { name, email, password }
             );
             const { data } = response;
