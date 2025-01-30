76a509df99e435822c93a2bff23abdc932c09c5f
Uzma-Aashiya <149761349+uzma-a@users.noreply.github.com>
2025-01-31 00:57:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURI, { useNewUrlParser: true})
   .then(() => {
     console.log('MongoDB Atlas connected');
   })
