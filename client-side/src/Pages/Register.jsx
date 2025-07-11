0893e52ebe154a61a69cdfc2f0e1f19acebc2a2f
Uzma-Aashiya <uzmaaasia444@gmail.com>
2025-07-11 19:00:31 +0530

    updata backend

--- client-side/src/Pages/Register.jsx
+++ client-side/src/Pages/Register.jsx
@@ -36,7 +36,7 @@ const Register = () => {
 
         try {
             const response = await axios.post(
-                'https://fithub-backend-0vzs.onrender.com/register',
+                'http://localhost:5001/api/register',
                 { name, email, password }
             );
             const { data } = response;
