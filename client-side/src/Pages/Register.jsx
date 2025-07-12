55249a27310148359457f3dcbca4cb5799267274
Uzma-Aashiya <149761349+uzma-a@users.noreply.github.com>
2025-07-12 12:47:48 +0530

    Update Register.jsx

--- client-side/src/Pages/Register.jsx
+++ client-side/src/Pages/Register.jsx
@@ -39,7 +39,7 @@ const Register = () => {
 
         try {
             const response = await axios.post(
-                `${backendUrl}/api/register`,
+                ${backendUrl}/api/register,
                 { name, email, password }
             );
             const { data } = response;
@@ -55,6 +55,93 @@ const Register = () => {
             console.error(error);
         }
     };
+
+    return (
+        <div
+            className="h-screen flex flex-col bg-cover bg-center bg-no-repeat bg-fixed justify-center relative"
+            style={{
+                backgroundImage: url(${bg_Img}),
+            }}
+        >
+            <div className="absolute inset-0 bg-black bg-opacity-80"></div>
+
+            <div className="flex items-center justify-between mb-6 px-6 md:px-20 relative z-10">
+                <Link to="/">
+                    <div
+                        id="logo-style"
+                        className="text-4xl sm:text-3xl font-bold text-white cursor-pointer"
+                    >
+                        FitHub
+                    </div>
+                </Link>
+            </div>
+
+            <div className="w-full sm:w-96 md:w-[420px] bg-[rgba(53,49,110,0.16)] p-6 sm:p-8 md:p-10 mx-auto relative z-10">
+                <h1 className="text-3xl sm:text-4xl font-semibold mb-6 text-center text-white">
+                    SignUp
+                </h1>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div>
+                        <label
+                            htmlFor="name"
+                            className="block text-xl font-medium text-gray-300"
+                        >
+                            Name
+                        </label>
+                        <input
+                            name="name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            type="text"
+                            placeholder="enter your name..."
+                            className="w-full h-12 bg-gray-600 text-white mb-3 border-none outline-none rounded-sm font-base text-lg px-4 py-2"
+                        />
+                    </div>
+                    <div>
+                        <label
+                            htmlFor="email"
+                            className="block text-xl font-medium text-gray-300"
+                        >
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="enter email..."
+                            name="email"
+                            className="w-full h-12 bg-gray-600 text-white mb-3 border-none outline-none rounded-sm font-base text-lg px-4 py-2"
+                        />
+                    </div>
+                    <div>
+                        <label
+                            htmlFor="password"
+                            className="block text-xl font-medium text-gray-300"
+                        >
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="enter password..."
+                            name="password"
+                            className="w-full h-12 bg-gray-600 text-white mb-3 border-none outline-none rounded-sm font-base text-lg px-4 py-2"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full border-none outline-none p-4 bg-cyan-800 text-white rounded-md text-lg font-medium mt-2 cursor-pointer"
+                    >
+                        Submit
+                    </button>
+                </form>
+
+            </div>
+        </div>
+    );
+};
+   
 
     return (
         <div
